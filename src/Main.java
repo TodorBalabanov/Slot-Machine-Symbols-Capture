2f739202813a6cceb1716ca8a9ddbf54239b66a7
Todor Balabanov <todorbalabanov@MACMINI.local>
2017-09-03 14:32:32 +0300

    Initial revision.

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Application single entry point class.
+ * 
+ * @author Todor Balabanov
+ */
+public class Main {
+
+	/**
+	 * Application single entry point method.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	}
+
+}
