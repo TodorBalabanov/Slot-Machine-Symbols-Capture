30c7a570e8deb6dbf69e65a27a100317ca6118f0
Todor Balabanov <todor.balabanov@gmail.com>
2017-09-05 14:52:36 +0300

    Symbols are transformed to colors.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.imageio.ImageIO;
 
@@ -16,9 +17,14 @@ import javax.imageio.ImageIO;
 public class Main {
 
 	/**
+	 * Symbol names.
+	 */
+	private static final String[] SYMBOLS = {"Dollar", "Apple", "Bannana", "Plum", "Cherry", "Grape", "Bar", "Seven", "Wild"};
+	
+	/**
 	 * Symbols coordinates.
 	 */
-	private static int coordinates[][] = { { 126, 190, 314, 330 }, { 336, 190, 524, 330 }, { 546, 190, 734, 330 },
+	private static final int COORDINATES[][] = { { 126, 190, 314, 330 }, { 336, 190, 524, 330 }, { 546, 190, 734, 330 },
 			{ 756, 190, 944, 330 }, { 966, 190, 1154, 330 }, { 126, 332, 314, 472 }, { 336, 332, 524, 472 },
 			{ 546, 332, 734, 472 }, { 756, 332, 944, 472 }, { 966, 332, 1154, 472 }, { 126, 474, 314, 614 },
 			{ 336, 474, 524, 614 }, { 546, 474, 734, 614 }, { 756, 474, 944, 614 }, { 966, 474, 1154, 614 },
@@ -54,6 +60,49 @@ public class Main {
 	}
 
 	/**
+	 * Capture symbols in particular image.
+	 * 
+	 * @param file
+	 *            Full file path and name.
+	 * 
+	 * @throws IOException
+	 *             Exception if there is a problem with the file.
+	 */
+	private static int[][] captureSymbols(String file, String result, int[][] indices) throws IOException {
+		Image input = ImageIO.read(new File(file));
+		// BufferedImage output = new BufferedImage(input.getWidth(null),
+		// input.getHeight(null),
+		// BufferedImage.TYPE_INT_RGB);
+		//
+		// Graphics g = output.getGraphics();
+		//
+		// g.setColor(Color.BLACK);
+		// g.fillRect(0, 0, output.getWidth(), output.getHeight());
+
+		int i = 0, j = 0;
+		for (int[] corners : COORDINATES) {
+			BufferedImage symbol = ((BufferedImage) input).getSubimage(corners[0], corners[1],
+					corners[2] - corners[0] + 1, corners[3] - corners[1] + 1);
+
+			indices[i][j] = averageColor(symbol);
+
+			// g.setColor(new Color(indices[i][j]));
+			// g.fillRect(corners[0], corners[1], corners[2] - corners[0] + 1,
+			// corners[3] - corners[1] + 1);
+
+			j++;
+			if (j >= indices[i].length) {
+				i++;
+				j = 0;
+			}
+		}
+
+		// ImageIO.write((RenderedImage) output, "png", new File(result));
+
+		return indices;
+	}
+
+	/**
 	 * Application single entry point method.
 	 * 
 	 * @param args
@@ -63,28 +112,14 @@ public class Main {
 	 *             Image file problem.
 	 */
 	public static void main(String[] args) throws IOException {
-		Image input = ImageIO.read(new File("./dat/00001.png"));
-		BufferedImage output = new BufferedImage(input.getWidth(null), input.getHeight(null),
-				BufferedImage.TYPE_INT_RGB);
-
-		Graphics g = output.getGraphics();
-
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, output.getWidth(), output.getHeight());
-
-		for (int[] corners : coordinates) {
-			BufferedImage symbol = ((BufferedImage) input).getSubimage(corners[0], corners[1],
-					corners[2] - corners[0] + 1, corners[3] - corners[1] + 1);
-
-			g.setColor(new Color(averageColor(symbol)));
-			g.fillRect(corners[0], corners[1], corners[2] - corners[0] + 1, corners[3] - corners[1] + 1);
-
-			// g.drawImage(input, corners[0], corners[1], corners[2],
-			// corners[3], corners[0], corners[1], corners[2],
-			// corners[3], null);
-		}
-
-		ImageIO.write((RenderedImage) output, "png", new File("./dat/example00001.png"));
+		System.out.println(Arrays.deepToString(captureSymbols("./dat/00001.png", "./dat/example00001.png",
+				new int[][] { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } })));
+		System.out.println(Arrays.deepToString(captureSymbols("./dat/00002.png", "./dat/example00002.png",
+				new int[][] { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } })));
+		System.out.println(Arrays.deepToString(captureSymbols("./dat/00003.png", "./dat/example00003.png",
+				new int[][] { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } })));
+		System.out.println(Arrays.deepToString(captureSymbols("./dat/00004.png", "./dat/example00004.png",
+				new int[][] { { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } })));
 	}
 
 }
