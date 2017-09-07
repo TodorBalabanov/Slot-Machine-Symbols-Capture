aaca333458d4b60776f1180fe68911a2db4a044c
Todor Balabanov <todor.balabanov@gmail.com>
2017-09-07 10:16:23 +0300

    Different ways to calculate average color were added.

--- src/Main.java
+++ src/Main.java
@@ -98,14 +98,33 @@ public class Main {
 	}
 
 	/**
+	 * Margin from the borders.
+	 */
+	private static final int MAGIN = 30;
+
+	/**
 	 * Symbols coordinates.
 	 */
-	private static final int COORDINATES[][] = { { 126, 190, 314, 330 }, { 336, 190, 524, 330 }, { 546, 190, 734, 330 },
-			{ 756, 190, 944, 330 }, { 966, 190, 1154, 330 }, { 126, 332, 314, 472 }, { 336, 332, 524, 472 },
-			{ 546, 332, 734, 472 }, { 756, 332, 944, 472 }, { 966, 332, 1154, 472 }, { 126, 474, 314, 614 },
-			{ 336, 474, 524, 614 }, { 546, 474, 734, 614 }, { 756, 474, 944, 614 }, { 966, 474, 1154, 614 },
-			{ 126, 616, 314, 756 }, { 336, 616, 524, 756 }, { 546, 616, 734, 756 }, { 756, 616, 944, 756 },
-			{ 966, 616, 1154, 756 }, };
+	private static final int COORDINATES[][] = { { 126 + MAGIN, 190 + MAGIN, 314 - MAGIN, 330 - MAGIN },
+			{ 336 + MAGIN, 190 + MAGIN, 524 - MAGIN, 330 - MAGIN },
+			{ 546 + MAGIN, 190 + MAGIN, 734 - MAGIN, 330 - MAGIN },
+			{ 756 + MAGIN, 190 + MAGIN, 944 - MAGIN, 330 - MAGIN },
+			{ 966 + MAGIN, 190 + MAGIN, 1154 - MAGIN, 330 - MAGIN },
+			{ 126 + MAGIN, 332 + MAGIN, 314 - MAGIN, 472 - MAGIN },
+			{ 336 + MAGIN, 332 + MAGIN, 524 - MAGIN, 472 - MAGIN },
+			{ 546 + MAGIN, 332 + MAGIN, 734 - MAGIN, 472 - MAGIN },
+			{ 756 + MAGIN, 332 + MAGIN, 944 - MAGIN, 472 - MAGIN },
+			{ 966 + MAGIN, 332 + MAGIN, 1154 - MAGIN, 472 - MAGIN },
+			{ 126 + MAGIN, 474 + MAGIN, 314 - MAGIN, 614 - MAGIN },
+			{ 336 + MAGIN, 474 + MAGIN, 524 - MAGIN, 614 - MAGIN },
+			{ 546 + MAGIN, 474 + MAGIN, 734 - MAGIN, 614 - MAGIN },
+			{ 756 + MAGIN, 474 + MAGIN, 944 - MAGIN, 614 - MAGIN },
+			{ 966 + MAGIN, 474 + MAGIN, 1154 - MAGIN, 614 - MAGIN },
+			{ 126 + MAGIN, 616 + MAGIN, 314 - MAGIN, 756 - MAGIN },
+			{ 336 + MAGIN, 616 + MAGIN, 524 - MAGIN, 756 - MAGIN },
+			{ 546 + MAGIN, 616 + MAGIN, 734 - MAGIN, 756 - MAGIN },
+			{ 756 + MAGIN, 616 + MAGIN, 944 - MAGIN, 756 - MAGIN },
+			{ 966 + MAGIN, 616 + MAGIN, 1154 - MAGIN, 756 - MAGIN }, };
 
 	/**
 	 * Calculate average color for the image.
@@ -114,11 +133,10 @@ public class Main {
 	 *            Image as input.
 	 * @return Average color as integer value.
 	 */
-	private static int averageColor(BufferedImage image) {
+	private static int averageColor1(BufferedImage image) {
 		long red = 0;
 		long green = 0;
 		long blue = 0;
-		long count = 0;
 
 		for (int y = 0; y < image.getHeight(); y++) {
 			for (int x = 0; x < image.getWidth(); x++) {
@@ -127,12 +145,115 @@ public class Main {
 				red += pixel.getRed();
 				green += pixel.getGreen();
 				blue += pixel.getBlue();
-
-				count++;
 			}
 		}
 
-		return (new Color((int) (red / count), (int) (green / count), (int) (blue / count))).getRGB();
+		double count = image.getWidth() * image.getHeight();
+		return (new Color((int) Math.round((red / count)), (int) Math.round((green / count)),
+				(int) Math.round((blue / count)))).getRGB();
+	}
+
+	/**
+	 * Calculate average color for the image.
+	 * 
+	 * @param image
+	 *            Image as input.
+	 * @return Average color as integer value.
+	 */
+	private static int averageColor2(BufferedImage image) {
+		long sum = 0;
+
+		for (int y = 0; y < image.getHeight(); y++) {
+			for (int x = 0; x < image.getWidth(); x++) {
+				sum += image.getRGB(x, y);
+			}
+		}
+
+		return (int) (sum /= image.getWidth() * image.getHeight());
+	}
+
+	/**
+	 * Calculate average color for the image.
+	 * 
+	 * @param image
+	 *            Image as input.
+	 * @return Average color as integer value.
+	 */
+	private static int averageColor3(BufferedImage image) {
+		long red = 0;
+		long green = 0;
+		long blue = 0;
+
+		for (int y = 0; y < image.getHeight(); y++) {
+			for (int x = 0; x < image.getWidth(); x++) {
+				Color pixel = new Color(image.getRGB(x, y));
+
+				red += pixel.getRed();
+				green += pixel.getGreen();
+				blue += pixel.getBlue();
+			}
+		}
+
+		return (int) Math.round((red + green + blue) / (3D * image.getWidth() * image.getHeight()));
+	}
+
+	/**
+	 * Calculate average color for the image.
+	 * 
+	 * @param image
+	 *            Image as input.
+	 * @return Average color as integer value.
+	 */
+	private static int averageColor4(BufferedImage image) {
+		double hue = 0;
+		double saturation = 0;
+		double brightness = 0;
+
+		float hsb[] = { 0, 0, 0 };
+
+		for (int y = 0; y < image.getHeight(); y++) {
+			for (int x = 0; x < image.getWidth(); x++) {
+				Color pixel = new Color(image.getRGB(x, y));
+				Color.RGBtoHSB(pixel.getRed(), pixel.getGreen(), pixel.getBlue(), hsb);
+
+				hue += hsb[0];
+				saturation += hsb[1];
+				brightness += hsb[2];
+			}
+		}
+
+		hue /= image.getWidth() * image.getHeight();
+		saturation /= image.getWidth() * image.getHeight();
+		brightness /= image.getWidth() * image.getHeight();
+
+		return (Color.getHSBColor((float) hue, (float) saturation, (float) brightness)).getRGB();
+	}
+
+	/**
+	 * Calculate average color for the image.
+	 * 
+	 * @param image
+	 *            Image as input.
+	 * @return Average color as integer value.
+	 */
+	private static int averageColor5(BufferedImage image) {
+		long red = 0;
+		long green = 0;
+		long blue = 0;
+
+		for (int y = 0; y < image.getHeight(); y++) {
+			for (int x = 0; x < image.getWidth(); x++) {
+				Color pixel = new Color(image.getRGB(x, y));
+
+				red += pixel.getRed();
+				green += pixel.getGreen();
+				blue += pixel.getBlue();
+			}
+		}
+
+		double count = image.getWidth() * image.getHeight();
+		return 1000000 * (int) Math.round((red / count)) + 1000 * (int) Math.round((green / count))
+				+ (int) Math.round((blue / count));
 	}
 
 	/**
@@ -152,7 +273,7 @@ public class Main {
 			BufferedImage symbol = ((BufferedImage) input).getSubimage(corners[0], corners[1],
 					corners[2] - corners[0] + 1, corners[3] - corners[1] + 1);
 
-			indices[i][j] = averageColor(symbol);
+			indices[i][j] = averageColor5(symbol);
 
 			j++;
 			if (j >= indices[i].length) {
