4c53a9944c351ebee3908f2a89e8d22baa4fd73b
todorbalabanov <todorbalabanov@192.168.0.102>
2017-10-11 14:58:55 +0300

    Better samples extraction.

--- src/Main.java
+++ src/Main.java
@@ -19,38 +19,38 @@ public class Main {
 	/**
 	 * Margin from the vertical borders.
 	 */
-	private static final int X_MAGIN = 30;
+	private static final int X_MARGIN = 0;
 
 	/**
 	 * Margin from the horizontal borders.
 	 */
-	private static final int Y_MAGIN = 10;
+	private static final int Y_MARGIN = 0;
 
 	/**
 	 * Symbols coordinates.
 	 */
 	private static final int COORDINATES[][] = {
 
-			{ 126 + X_MAGIN, 190 + Y_MAGIN, 314 - X_MAGIN, 330 - Y_MAGIN },
-			{ 336 + X_MAGIN, 190 + Y_MAGIN, 524 - X_MAGIN, 330 - Y_MAGIN },
-			{ 546 + X_MAGIN, 190 + Y_MAGIN, 734 - X_MAGIN, 330 - Y_MAGIN },
-			{ 756 + X_MAGIN, 190 + Y_MAGIN, 944 - X_MAGIN, 330 - Y_MAGIN },
-			{ 966 + X_MAGIN, 190 + Y_MAGIN, 1154 - X_MAGIN, 330 - Y_MAGIN },
-			{ 126 + X_MAGIN, 332 + Y_MAGIN, 314 - X_MAGIN, 472 - Y_MAGIN },
-			{ 336 + X_MAGIN, 332 + Y_MAGIN, 524 - X_MAGIN, 472 - Y_MAGIN },
-			{ 546 + X_MAGIN, 332 + Y_MAGIN, 734 - X_MAGIN, 472 - Y_MAGIN },
-			{ 756 + X_MAGIN, 332 + Y_MAGIN, 944 - X_MAGIN, 472 - Y_MAGIN },
-			{ 966 + X_MAGIN, 332 + Y_MAGIN, 1154 - X_MAGIN, 472 - Y_MAGIN },
-			{ 126 + X_MAGIN, 474 + Y_MAGIN, 314 - X_MAGIN, 614 - Y_MAGIN },
-			{ 336 + X_MAGIN, 474 + Y_MAGIN, 524 - X_MAGIN, 614 - Y_MAGIN },
-			{ 546 + X_MAGIN, 474 + Y_MAGIN, 734 - X_MAGIN, 614 - Y_MAGIN },
-			{ 756 + X_MAGIN, 474 + Y_MAGIN, 944 - X_MAGIN, 614 - Y_MAGIN },
-			{ 966 + X_MAGIN, 474 + Y_MAGIN, 1154 - X_MAGIN, 614 - Y_MAGIN },
-			{ 126 + X_MAGIN, 616 + Y_MAGIN, 314 - X_MAGIN, 756 - Y_MAGIN },
-			{ 336 + X_MAGIN, 616 + Y_MAGIN, 524 - X_MAGIN, 756 - Y_MAGIN },
-			{ 546 + X_MAGIN, 616 + Y_MAGIN, 734 - X_MAGIN, 756 - Y_MAGIN },
-			{ 756 + X_MAGIN, 616 + Y_MAGIN, 944 - X_MAGIN, 756 - Y_MAGIN },
-			{ 966 + X_MAGIN, 616 + Y_MAGIN, 1154 - X_MAGIN, 756 - Y_MAGIN },
+			{ 126 + X_MARGIN, 190 + Y_MARGIN, 314 - X_MARGIN, 330 - Y_MARGIN },
+			{ 336 + X_MARGIN, 190 + Y_MARGIN, 524 - X_MARGIN, 330 - Y_MARGIN },
+			{ 546 + X_MARGIN, 190 + Y_MARGIN, 734 - X_MARGIN, 330 - Y_MARGIN },
+			{ 756 + X_MARGIN, 190 + Y_MARGIN, 944 - X_MARGIN, 330 - Y_MARGIN },
+			{ 966 + X_MARGIN, 190 + Y_MARGIN, 1154 - X_MARGIN, 330 - Y_MARGIN },
+			{ 126 + X_MARGIN, 332 + Y_MARGIN, 314 - X_MARGIN, 472 - Y_MARGIN },
+			{ 336 + X_MARGIN, 332 + Y_MARGIN, 524 - X_MARGIN, 472 - Y_MARGIN },
+			{ 546 + X_MARGIN, 332 + Y_MARGIN, 734 - X_MARGIN, 472 - Y_MARGIN },
+			{ 756 + X_MARGIN, 332 + Y_MARGIN, 944 - X_MARGIN, 472 - Y_MARGIN },
+			{ 966 + X_MARGIN, 332 + Y_MARGIN, 1154 - X_MARGIN, 472 - Y_MARGIN },
+			{ 126 + X_MARGIN, 474 + Y_MARGIN, 314 - X_MARGIN, 614 - Y_MARGIN },
+			{ 336 + X_MARGIN, 474 + Y_MARGIN, 524 - X_MARGIN, 614 - Y_MARGIN },
+			{ 546 + X_MARGIN, 474 + Y_MARGIN, 734 - X_MARGIN, 614 - Y_MARGIN },
+			{ 756 + X_MARGIN, 474 + Y_MARGIN, 944 - X_MARGIN, 614 - Y_MARGIN },
+			{ 966 + X_MARGIN, 474 + Y_MARGIN, 1154 - X_MARGIN, 614 - Y_MARGIN },
+			{ 126 + X_MARGIN, 616 + Y_MARGIN, 314 - X_MARGIN, 756 - Y_MARGIN },
+			{ 336 + X_MARGIN, 616 + Y_MARGIN, 524 - X_MARGIN, 756 - Y_MARGIN },
+			{ 546 + X_MARGIN, 616 + Y_MARGIN, 734 - X_MARGIN, 756 - Y_MARGIN },
+			{ 756 + X_MARGIN, 616 + Y_MARGIN, 944 - X_MARGIN, 756 - Y_MARGIN },
+			{ 966 + X_MARGIN, 616 + Y_MARGIN, 1154 - X_MARGIN, 756 - Y_MARGIN },
 
 	};
 
@@ -203,18 +203,43 @@ public class Main {
 	 *            Name of the result file if images are stored as output.
 	 * @param letters
 	 *            The letters of the symbols on the screen.
+	 * 
+	 * @return Reference to the array with the letters on the screen.
+	 * 
+	 * @throws IOException
+	 *             Exception if there is a problem with the file.
+	 */
+	private static void captureSamples(String file, String result) throws IOException {
+		BufferedImage input = ImageIO.read(new File(file));
+
+		BufferedImage output = new BufferedImage(input.getWidth(null), input.getHeight(null), input.getType());
+		Graphics g = output.getGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, output.getWidth(), output.getHeight());
+
+		for (int[] corners : COORDINATES) {
+			BufferedImage symbol = ((BufferedImage) input).getSubimage(corners[0], corners[1], corners[2] - corners[0],
+					corners[3] - corners[1]);
+
+			g.drawImage(input, corners[0], corners[1], corners[2], corners[3], corners[0], corners[1], corners[2],
+					corners[3], null);
+		}
+
+		ImageIO.write((RenderedImage) output, "png", new File(result));
+	}
+
+	/**
+	 * 
+	 * @param file
+	 *            Full input image file path and name.
+	 * @param letters
+	 *            The letters of the symbols on the screen.
 	 * @return Reference to the array with the letters on the screen.
 	 * @throws IOException
 	 *             Exception if there is a problem with the file.
 	 */
-	private static String[][] captureSymbols(String file, String result, String[][] letters) throws IOException {
+	private static String[][] captureSymbols(String file, String[][] letters) throws IOException {
 		BufferedImage input = ImageIO.read(new File(file));
-
-		// BufferedImage output = new BufferedImage(input.getWidth(null),
-		// input.getHeight(null), input.getType());
-		// Graphics g = output.getGraphics();
-		// g.setColor(Color.BLACK);
-		// g.fillRect(0, 0, output.getWidth(), output.getHeight());
 
 		int i = 0, j = 0;
 		for (int[] corners : COORDINATES) {
@@ -223,10 +248,6 @@ public class Main {
 
 			letters[i][j] = "" + similarity(symbol);
 
-			// g.drawImage(input, corners[0], corners[1], corners[2], corners[3],
-			// corners[0], corners[1], corners[2],
-			// corners[3], null);
-
 			j++;
 			if (j >= letters[i].length) {
 				i++;
@@ -234,29 +255,17 @@ public class Main {
 			}
 		}
 
-		// ImageIO.write((RenderedImage) output, "png", new File(result));
-
 		return letters;
 	}
 
 	/**
-	 * Application single entry point method.
-	 * 
-	 * Command which can be used to start the application:
-	 * 
-	 * java -cp ./bin/ Main >./bin/out.txt & disown
-	 * 
-	 * @param args
-	 *            Command line arguments.
+	 * Obtain symbols as samples in image files.
 	 * 
 	 * @throws IOException
-	 *             Image file problem.
+	 *             Procedure end if there is a input/output problem.
 	 */
-	public static void main(String[] args) throws IOException {
-		String pieces[] = { "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-				"", "" };
-
-		for (File file : (new File("./dat")).listFiles()) {
+	private static void samples() throws IOException {
+		for (File file : (new File("./data/examples")).listFiles()) {
 			if (file.isFile() == false) {
 				continue;
 			}
@@ -265,11 +274,33 @@ public class Main {
 				continue;
 			}
 
-			String[][] view = captureSymbols(file.getPath(),
+			captureSamples(file.getPath(),
 					file.getPath().substring(0, file.getPath().length() - file.getName().length()) + "result"
-							+ file.getName(),
-					new String[][] { { "", "", "", "", "" }, { "", "", "", "", "" }, { "", "", "", "", "" },
-							{ "", "", "", "", "" } });
+							+ file.getName());
+		}
+	}
+
+	/**
+	 * Processing of the images.
+	 * 
+	 * @throws IOException
+	 *             Procedure end if there is a input/output problem.
+	 */
+	private static void process() throws IOException {
+		String pieces[] = { "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+				"", "" };
+
+		for (File file : (new File("./data/input")).listFiles()) {
+			if (file.isFile() == false) {
+				continue;
+			}
+
+			if (file.getName().contains(".png") == false) {
+				continue;
+			}
+
+			String[][] view = captureSymbols(file.getPath(), new String[][] { { "", "", "", "", "" },
+					{ "", "", "", "", "" }, { "", "", "", "", "" }, { "", "", "", "", "" } });
 
 			for (int j = 0; j < view[0].length; j++) {
 				for (int i = 0; i < view.length; i++) {
@@ -286,6 +317,24 @@ public class Main {
 		for (String value : pieces) {
 			System.out.println(value);
 		}
+	}
+
+	/**
+	 * Application single entry point method.
+	 * 
+	 * Command which can be used to start the application:
+	 * 
+	 * java -cp ./bin/ Main >./bin/out.txt & disown
+	 * 
+	 * @param args
+	 *            Command line arguments.
+	 * 
+	 * @throws IOException
+	 *             Image file problem.
+	 */
+	public static void main(String[] args) throws IOException {
+		samples();
+		// process();
 	}
 
 }
